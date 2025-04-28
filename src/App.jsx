db44eda8e292ac36199cff18d0f0ad5bbe781e3d
BerkantAhmed <belgin_berk@abv.bg>
2025-04-28 13:17:14 +0300

    Added ProtectedRoute to secure dashboard acces

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
+import ProtectedRoute from './ProtectedRoute';
 function App() {
   return (
    
@@ -17,7 +18,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<ProtectedRoute> <Dashboard /> </ProtectedRoute>
+} />
       </Routes>
     </Router>
       <header className="App-header">
